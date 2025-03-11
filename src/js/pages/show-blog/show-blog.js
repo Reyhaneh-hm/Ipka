83b1e67602c9fa8cc5e4c6f175bb97d6ce902b0f
Reyhaneh-hm <reyhaneh3784@gmail.com>
2025-03-11 10:46:38 +0330

    Add show-blog page and style pagination

---
+++ src/js/pages/show-blog/show-blog.js
@@ -0,0 +1,18 @@
+import '../../../scss/components/pages/show-blog/index.scss';
+
+/*--------------show filter------------*/
+document.addEventListener("DOMContentLoaded", function () {
+    const filterBtn = document.querySelector(".filterBtn");
+    const modalFilter = document.querySelector(".modalFilter");
+    const overlay = document.createElement("div");
+
+    overlay.classList.add("overlay2");
+    document.body.appendChild(overlay);
+
+    filterBtn.addEventListener("click", () => {
+        [overlay, modalFilter].forEach((item) => item.classList.add("active"));
+    });
+    overlay.addEventListener("click", () => {
+        [overlay, modalFilter].forEach((item) => item.classList.remove("active"));
+    });
+});
