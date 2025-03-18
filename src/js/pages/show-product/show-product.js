a306c43a9bda044f440d0d172540bc1011c04b44
Reyhaneh-hm <reyhaneh3784@gmail.com>
2025-03-18 22:21:52 +0330

    Add video

--- src/js/pages/show-product/show-product.js
+++ src/js/pages/show-product/show-product.js
@@ -88,12 +88,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /*-------------------images and FancyBox----------------------*/
 
+// document.addEventListener("DOMContentLoaded", function () {
+//     const bigImageLink = document.querySelector(".big-img");
+//     const bigImage = bigImageLink.querySelector(".img");
+//     const galleryItems = document.querySelectorAll(".gallery .item");
+//     const lastItem = galleryItems[galleryItems.length - 1];
+//     const videoURL = "https://www.aparat.com/v/Xn02c?t=0";
+
+//     /*--------------image product--------------*/
+//     galleryItems.forEach(item => {
+//         item.addEventListener("click", function () {
+//             galleryItems.forEach(el => el.classList.remove("active"));
+//             this.classList.add("active");
+
+//             bigImageLink.innerHTML = "";
+
+//             if (this === lastItem) {
+//                 bigImageLink.innerHTML = `
+//                     <video class="video" controls autoplay>
+//                         <source src="${videoURL}" type="video/mp4">
+//                         مرورگر شما از ویدیو پشتیبانی نمی‌کند.
+//                     </video>
+//                 `;
+//                 bigImageLink.removeAttribute("href");
+//             } else {
+//                 const newSrc = this.getAttribute("data-src");
+//                 bigImageLink.innerHTML = `<img class="img" src="${newSrc}" alt="">`;
+//                 bigImageLink.setAttribute("href", newSrc);
+//             }
+//         });
+//     });
+
+//     /*--------------FancyBox--------------*/
+//     document.addEventListener("click", function (event) {
+//         if (event.target.closest("[data-fancybox='product']")) {
+//             Fancybox.getInstance()?.on("Carousel.change", (fancybox, carousel, slide) => {
+//                 const newSrc = slide.src;
+
+//                 bigImageLink.innerHTML = `<img class="img" src="${newSrc}" alt="">`;
+//                 bigImageLink.setAttribute("href", newSrc);
+
+//                 galleryItems.forEach(item => {
+//                     const img = item.querySelector("img");
+//                     if (img && img.getAttribute("src") === newSrc) {
+//                         galleryItems.forEach(el => el.classList.remove("active"));
+//                         item.classList.add("active");
+//                     }
+//                 });
+//             });
+//         }
+//     });
+// });
 document.addEventListener("DOMContentLoaded", function () {
     const bigImageLink = document.querySelector(".big-img");
     const bigImage = bigImageLink.querySelector(".img");
     const galleryItems = document.querySelectorAll(".gallery .item");
-    const lastItem = galleryItems[galleryItems.length - 1];
-    const videoURL = "https://www.aparat.com/v/Xn02c?t=0";
 
     /*--------------image product--------------*/
     galleryItems.forEach(item => {
@@ -103,39 +152,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
             bigImageLink.innerHTML = "";
 
-            if (this === lastItem) {
+            if (this.classList.contains("video-item")) {
+                const videoURL = this.getAttribute("data-src");
                 bigImageLink.innerHTML = `
-                    <video class="video" controls autoplay>
-                        <source src="${videoURL}" type="video/mp4">
-                        مرورگر شما از ویدیو پشتیبانی نمی‌کند.
-                    </video>
+                    <iframe class="video" src="${videoURL}" frameborder="0" allowfullscreen></iframe>
                 `;
-                bigImageLink.removeAttribute("href");
             } else {
                 const newSrc = this.getAttribute("data-src");
                 bigImageLink.innerHTML = `<img class="img" src="${newSrc}" alt="">`;
-                bigImageLink.setAttribute("href", newSrc);
             }
         });
     });
 
     /*--------------FancyBox--------------*/
-    document.addEventListener("click", function (event) {
-        if (event.target.closest("[data-fancybox='product']")) {
-            Fancybox.getInstance()?.on("Carousel.change", (fancybox, carousel, slide) => {
-                const newSrc = slide.src;
-
-                bigImageLink.innerHTML = `<img class="img" src="${newSrc}" alt="">`;
-                bigImageLink.setAttribute("href", newSrc);
-
-                galleryItems.forEach(item => {
-                    const img = item.querySelector("img");
-                    if (img && img.getAttribute("src") === newSrc) {
-                        galleryItems.forEach(el => el.classList.remove("active"));
-                        item.classList.add("active");
-                    }
-                });
-            });
-        }
+    Fancybox.bind("[data-fancybox='product']", {
+        infinite: true,
+        keyboard: true,
+        loop: true,
+        buttons: [
+            'zoom',
+            'slideShow',
+            'fullScreen',
+            'download',
+            'thumbs',
+            'close'
+        ],
+        animationEffect: "zoom",
+        transitionEffect: "slide",
+        thumbs: {
+            autoStart: true
+        },
+        keyboard: true,
+        touch: true,
+        protect: true,
+        idleTime: false,
+        smallBtn: true,
+        toolbar: true,
+        animationDuration: 500,
+        transitionDuration: 600,
     });
 });
