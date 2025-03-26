aa7ce0a4f2b386e7410d187e4ce461403c5fb693
helal92 <m.koochaki@fartech.ir>
2025-03-26 08:48:15 +0330

    fix style 2

--- src/js/pages/order/order.js
+++ src/js/pages/order/order.js
@@ -10,7 +10,9 @@ numberSpinner.forEach((item) => {
     const maxValue = input.getAttribute("max");
     const stepValue = input.getAttribute("step");
 
-    input.value = minValue;
+    if(input.value != '') {
+        input.value = minValue;
+    }
 
     increament.addEventListener("click", () => {
         if (+input.value < +maxValue) {
